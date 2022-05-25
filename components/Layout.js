e1e7669cf24aabc093f8ad7952bf1b43a31245e4
alzamiarafat <alzamiarafat00@gmail.com>
2022-05-25 18:43:44 +0600

    navbar desigign

---
+++ components/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+export default function Layout({ children }) {
+    return (
+        <div>
+            <main>
+                <Navbar />
+                {children}
+            </main>
+
+        </div>
+    )
+}
